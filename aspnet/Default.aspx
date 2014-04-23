1da3af8c320fb8dca575ba60a7eed906933cc4e9
Fsmart <ggphao@gmail.com>
2014-04-23 11:31:03 +0800

    asp网站

---
+++ aspnet/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>无标题页</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        &nbsp;
+        <asp:FileUpload ID="FileUpload1" runat="server"  Width = "306px"/><br />&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="上传文件" /><br />
+        <asp:Label ID="Label1" runat="server"  Font-Bold = "true" Font-Size = "10pt" ForeColor = " #ff3333" Width = "199px"></asp:Label>
+        </div>
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="207px" Width="299px" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="浏览" />
+           </div>
+         <div>
+             <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True">
+                 <asp:ListItem Value="0">hah</asp:ListItem>
+                 <asp:ListItem Value="1">hello world</asp:ListItem>
+                 <asp:ListItem Value="2">world hello</asp:ListItem>
+                 <asp:ListItem Value="3"></asp:ListItem>
+             </asp:RadioButtonList><br />
+             <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Button" /><br />
+             <asp:Label ID="Label2" runat="server" ></asp:Label></div>
+    </form>
+</body>
+</html>
+
---
+++ aspnet/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    static int i = 1;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Image1.ImageUrl = "~\\image\\Blue hills.jpg";
+    }
+   
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            try
+            {
+                FileUpload1.PostedFile.SaveAs("e:\\date\\" + FileUpload1.FileName);
+                Label1.Text = "上传成功";
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "上传失败";
+            }
+        }
+      
+    }
+    
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+        if (i == 1)
+        {
+            Image1.ImageUrl = "~\\image\\Blue hills.jpg";
+            i=2;
+        }
+        else if (i ==2)
+        {
+            Image1.ImageUrl = "~\\image\\Sunset.jpg";
+            i=3;
+        }
+        else if (i == 3)
+        {
+            Image1.ImageUrl = "~\\image\\Water lilies.jpg";
+            i=4;
+        }
+       else if(i==4)
+        {
+            Image1.ImageUrl = "~\\image\\Winter.jpg";
+            i=1;
+
+        }
+
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (RadioButtonList1.SelectedIndex == 0)
+            Label2.Text = "heeh";
+        else if(RadioButtonList1.SelectedIndex == 1)
+             Label2.Text = "haha";
+            else if(RadioButtonList1.SelectedIndex == 2)
+             Label2.Text = "heihei";
+    }
+}
